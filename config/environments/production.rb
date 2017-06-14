1396d753aa4a13b321be99e607ab5f0516288db0
komoriyuichiro <cmr@c-mbp.local>
2017-06-14 15:45:42 +0900

    mailer system final

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,8 +67,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['app70182876@heroku.com'],
-    :password       => ENV['jade5kpa9115'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
